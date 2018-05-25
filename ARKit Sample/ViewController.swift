b74a3355dbad99f57af9aee3237c0c7b62cb7ac2
slade9220 <slade9220@gmail.com>
2018-05-25 10:42:09 +0200

    IV commit

--- ARKit Sample/ViewController.swift
+++ ARKit Sample/ViewController.swift
@@ -18,19 +18,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return true
     }
     
-    override func prefersHomeIndicatorAutoHidden() -> Bool {
-        return true
-    }
-    
-    var planeType = planeDetection.horizontal
+    var planeType = planeDetection.image
     
     @IBOutlet weak var planeButton: UIButton!
+    @IBOutlet weak var imageRecognition: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -62,11 +56,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             configuration.planeDetection = .vertical
         }
         if plane == planeDetection.image {
-            
+            guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
+            configuration.detectionImages = referenceImages
         }
         
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
+    
+    
+    @IBAction func imageRecognitionPressed(_ sender: Any) {
+        
+        if planeType != planeDetection.image {
+            planeType = planeDetection.image
+            setupSession(plane: planeType)
+            return
+        } else {
+            planeType = planeDetection.image
+            setupSession(plane: planeType)
+        }
+        
+    }
+    
     
     @IBAction func planeButtonPressed(_ sender: Any) {
         
@@ -85,11 +95,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        
-        
-        
         if  planeType == planeDetection.horizontal {
+            guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
             let width = CGFloat(planeAnchor.extent.x)
             let height = CGFloat(planeAnchor.extent.z)
             let plane = SCNPlane(width: width, height: height)
@@ -107,6 +114,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             node.addChildNode(planeNode)
         }
         if  planeType == planeDetection.vertical {
+            guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
             let width = CGFloat(planeAnchor.extent.z)
             let height = CGFloat(planeAnchor.extent.y)
             let plane = SCNPlane(width: width, height: height)
@@ -125,35 +133,59 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             print(planeNode)
             node.addChildNode(planeNode)
         }
-
+        
+        if planeType == planeDetection.image {
+            
+            guard let imageAnchor = anchor as? ARImageAnchor else { return }
+            let referenceImage = imageAnchor.referenceImage
+            
+            let plane = SCNPlane(width: referenceImage.physicalSize.width, height: referenceImage.physicalSize.height)
+            plane.materials.first?.diffuse.contents = UIColor.gray.withAlphaComponent(0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(planeNode)
+        }
+        
+        
+        
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+         if planeType == planeDetection.image {
+            
+            return
+            
+         } else {
+            
+            guard let planeAnchor = anchor as?  ARPlaneAnchor,
+                let planeNode = node.childNodes.first,
+                let plane = planeNode.geometry as? SCNPlane
+                else { return }
+            
+            if planeType == planeDetection.horizontal {
+                let width = CGFloat(planeAnchor.extent.x)
+                let height = CGFloat(planeAnchor.extent.z)
+                plane.width = width
+                plane.height = height
+            }
+            
+            if planeType == planeDetection.vertical {
+                let width = CGFloat(planeAnchor.extent.z)
+                let height = CGFloat(planeAnchor.extent.y)
+                plane.width = width
+                plane.height = height
+            }
+            
+            
+            let x = CGFloat(planeAnchor.center.x)
+            let y = CGFloat(planeAnchor.center.y)
+            let z = CGFloat(planeAnchor.center.z)
+            planeNode.position = SCNVector3(x, y, z)
+        }
        
-        guard let planeAnchor = anchor as?  ARPlaneAnchor,
-            let planeNode = node.childNodes.first,
-            let plane = planeNode.geometry as? SCNPlane
-            else { return }
         
-        if planeType == planeDetection.horizontal {
-            let width = CGFloat(planeAnchor.extent.x)
-            let height = CGFloat(planeAnchor.extent.z)
-            plane.width = width
-            plane.height = height
-        }
-        
-        if planeType == planeDetection.vertical {
-            let width = CGFloat(planeAnchor.extent.z)
-            let height = CGFloat(planeAnchor.extent.y)
-            plane.width = width
-            plane.height = height
-        }
-        
-        
-        let x = CGFloat(planeAnchor.center.x)
-        let y = CGFloat(planeAnchor.center.y)
-        let z = CGFloat(planeAnchor.center.z)
-        planeNode.position = SCNVector3(x, y, z)
         
     }
     
